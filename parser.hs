acffe81ac0b6b8dfc5dd3e59c138cdf4d249a0dc
Shrivatsa <shrivatsa.swadi@gmail.com>
2020-09-30 21:01:01 +0530

    Added some String parsing

--- parser.hs
+++ parser.hs
@@ -1,3 +1,4 @@
+-- Some solutions copied from https://github.com/dstcruz/Write-Yourself-A-Scheme-In-48-Hours
 import Text.ParserCombinators.Parsec hiding (spaces)
 import Control.Monad
 import System.Environment
@@ -23,9 +24,20 @@ data LispVal = Atom String
 parseString :: Parser LispVal
 parseString = do
                 char '"'
-                x <- many (noneOf "\"")
+                x <- many (escapedChars <|> noneOf "\"")
                 char '"'
                 return $ String x
+
+escapedChars :: Parser Char
+escapedChars = do
+             char '\\'
+             c <- oneOf ['\\','"', 'n', 'r', 't']
+             return $ case c of
+                 '\\' -> c
+                 '"'  -> c
+                 'n'  -> '\n'
+                 'r'  -> '\r'
+                 't'  -> '\t'
 
 parseAtom :: Parser LispVal
 parseAtom = do 
@@ -37,8 +49,16 @@ parseAtom = do
                          "#f" -> Bool False
                          _    -> Atom atom
 
+-- parseNumber :: Parser LispVal
+-- parseNumber = liftM (Number . read) $ many1 digit
+
 parseNumber :: Parser LispVal
-parseNumber = liftM (Number . read) $ many1 digit
+parseNumber = (many1 digit) >>= (\x -> (return $ (Number . read) x))
+
+-- parseNumber :: Parser LispVal
+-- parseNumber = do 
+--                 x <- many1 digit
+--                 return $ (Number . read) x
 
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
